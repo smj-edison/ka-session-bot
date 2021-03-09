e56ec7d000b4e178a36d310842d678a4f00d6a1d
smj-edison <stephen.mason.jones@gmail.com>
2021-03-08 19:51:06 -0800

    WIP

---
+++ src/loading/loadPng.js
@@ -0,0 +1,24 @@
+const axios = require("axios");
+const {PNG} = require("pngjs");
+
+async function loadPng(url) {
+    return new Promise((resolve, reject) => {
+
+    });
+    
+    var png = new PNG({
+        filterType: -1
+    });
+
+    axios({
+        method: 'get',
+        url: url,
+        responseType: 'stream'
+    }).then(response => {
+        response.data.pipe(png);
+    });
+
+    png.on("parsed", () => {
+        console.log(this.data);
+    });
+}
