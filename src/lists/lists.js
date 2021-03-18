54d67ac7fe8a6c382f14941d24b42d59392c8990
smj-edison <stephen.mason.jones@gmail.com>
2021-03-17 20:51:08 -0700

    Add lists

--- src/lists/lists.js
+++ src/lists/lists.js
@@ -1,18 +1,46 @@
-function getList() {
-    return {
-        list: []
-    };
+const multiFilter = require("../util/multiFilter.js");
+
+function List() {
+    this.list = [];
 }
 
-function makeListTimeout(list, timeoutTime) {
-    list.timeout = [];
-    list.timeoutTime = timeoutTime;
-
-    return list;
+List.prototype.add = function(item) {
+    return this.list.push(item) - 1;
 }
+
+List.prototype.remove = function(index) {
+    this.list.splice(index, 1);
+}
+
+
+
+function TimeoutList(timeoutTime) {
+    List.call(this);
+
+    this.timeouts = [];
+    this.timeoutTime = timeoutTime;
+}
+
+TimeoutList.prototype = Object.create(List.prototype);
+
+TimeoutList.prototype.add = function(item) {
+    const index = List.prototype.add.call(this, item);
+
+    this.timeouts[index] = (new Date()).getMilliseconds() + this.timeoutTime;
+};
+
+TimeoutList.prototype.remove = function(index) {
+    List.prototype.remove.call(this, index);
+    this.timeouts.splice(index, 1);
+}
+
+TimeoutList.prototype.purgeExpired = function() {
+    const now = (new Date()).getMilliseconds();
+    [this.timeouts, this.list] = multiFilter([this.timeouts, this.list], expireTime => now >= expireTime);
+};
 
 
 module.exports = {
-    getList,
-    makeListTimeout
+    List,
+    TimeoutList
 };
